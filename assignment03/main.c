2401db3a4260a2eb8ebf743521f74ce2c281f5a2
iaschnei <iaschnei@student.42.fr>
2026-03-18 14:56:05 +0100

    Assignment 3

---
+++ assignment03/main.c
@@ -0,0 +1,36 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+
+static int do_work(int *my_int) 
+{
+	int x;
+	int y = *my_int;
+	int z;
+
+	for (x = 0; x < y; y++) 
+		udelay(10);
+
+	if (y > 10)
+		pr_info("We slept a long time!");
+
+	z = x * y;
+
+	return z;
+}
+
+static int __init my_init(void)
+{
+	int x = 10;
+
+	x = do_work(&x);
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
